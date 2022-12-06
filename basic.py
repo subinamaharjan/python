879958736a7e02deb01ad63eede2735cf5c73b0f
subina <subinamaharjan@540gmail.com>
2022-12-06 13:35:49 +0545

    msg

---
+++ basic.py
@@ -0,0 +1,10 @@
+mark = [10,20,30,40,50,60,70]
+#mark.append(10)
+#print(mark)
+#mark [0] = 100
+#print(mark)
+#mark.clear()
+#mark.insert(3,36)
+#mark.remove(50) we take a value
+mark.pop(2) #we take index
+print(mark)
