6699896cdf0ac01b9bbe24ea357f93c628538c03
subina <subinamaharjan@540gmail.com>
2022-12-13 09:01:04 +0545

    basic

--- basic.py
+++ basic.py
@@ -1,4 +1,4 @@
-mark = [10,20,30,40,50,60,70]
+#mark = [10,20,30,40,50,60,70]
 #mark.append(10)
 #print(mark)
 #mark [0] = 100
@@ -6,5 +6,23 @@ mark = [10,20,30,40,50,60,70]
 #mark.clear()
 #mark.insert(3,36)
 #mark.remove(50) we take a value
-mark.pop(2) #we take index
-print(mark)
+##mark.pop(2) #we take index
+#print(mark)
+
+#pi = 3.4
+#r = int(input("enter the radius of circle"))
+#print(f"{r}")
+#area = 1/2 * pi * r*r
+#print(f"the area of circle is{area}")
+
+#mark = [10,20,30,40,50]
+#mark.insert(2,25)
+#print(mark)
+
+#tuple
+
+tp = ('sunday','monday','tuesday','wednesday','Thurday','Friday')
+
+
+tp [0] = 'saturday'#in tuple we cant change the value or can't do item assignment
+print(tp)
